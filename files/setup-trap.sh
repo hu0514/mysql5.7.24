b3ced9b1eec4e434d264cd3f7017a0932861bda8
huliyuan <1031393617@qq.com>
2019-01-15 13:27:43 +0800

    从trap传递信号改为exec传递信号

--- files/setup-trap.sh
+++ files/setup-trap.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+pid=0
+term()
+{
+        echo $pid
+	kill -TERM "$pid"
+	wait "$pid"
+	exit ;
+}
+trap term TERM
 lock=`cat /usr/local/mysql/conf/my.cnf|grep socket|awk -F"=" '{print $2}'|head -n 1`.lock
 if [ "`ls -A /home/mysqldata`" = "" ];then
 	/usr/local/mysql/bin/mysqld --initialize  --user=mysql --basedir=/usr/local/mysql --datadir=/home/mysqldata >> /tmp/mysql.log 2>&1
-	/usr/local/mysql/bin/mysqld_safe &
-	sleep 5
+	#/usr/local/mysql/bin/mysqld_safe &
+	/usr/local/mysql/bin/mysqld --user=mysql &
+	sleep 10
 	mysqladmin -uroot -p`cat /tmp/mysql.log |grep "root@localhost"|awk -F:" " {'print $2'}` password 111111
-	mysql -uroot -p111111 -e "create user 'root'@'%' identified by'111111';"
-	mysql -uroot -p111111 -e "grant all privileges on *.* to 'root'@'%' with grant option;"
-	mysql -uroot -p111111 -e "flush privileges;"
-	/usr/local/mysql/support-files/mysql.server stop
+	kill -9 `ps -ef|grep -v 'grep'|grep mysqld|awk -F" " {'print $2'}`
 fi
 if [ -f $lock ];then
 	rm -f $lock
 fi
-exec /usr/local/mysql/bin/mysqld 
+/usr/local/mysql/bin/mysqld  --defaults-file=/usr/local/mysql/conf/my.cnf --user=mysql & pid="$!"
+while true
+do
+tail -f /dev/null & wait ${!}
+done
